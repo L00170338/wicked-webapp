186fb60d1bde82df37c767a28ff439d9cedc5b2e
Wagner Ribeiro <L00170338@student.lyit.ie>
2022-03-28 00:11:08 +0000

    Modified DB

--- config/settings.py
+++ config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 if os.getenv('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
         }
     }
 else:
@@ -96,6 +100,7 @@ else:
             'PORT': '5432'
         }
     }
+
  
 #DATABASES = {
 #    'default': {
